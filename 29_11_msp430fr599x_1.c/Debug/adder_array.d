22cf2c4afb75e183e682076a9ad6550b99bb188b
ganrajborade <boradeganraj@gmail.com>
2023-11-29 19:24:35 +0530

    added time calculation based on msp frequency

---
+++ 29_11_msp430fr599x_1.c/Debug/adder_array.d
@@ -0,0 +1,24 @@
+# FIXED
+
+adder_array.obj: ../adder_array.c
+adder_array.obj: /home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/msp430.h
+adder_array.obj: /home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/msp430fr5994.h
+adder_array.obj: /home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/in430.h
+adder_array.obj: /home/ganraj/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+adder_array.obj: /home/ganraj/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+adder_array.obj: /home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/legacy.h
+
+../adder_array.c:
+
+/home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/msp430.h:
+
+/home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/msp430fr5994.h:
+
+/home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/in430.h:
+
+/home/ganraj/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/home/ganraj/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/home/ganraj/ti/ccs1250/ccs/ccs_base/msp430/include/legacy.h:
+
